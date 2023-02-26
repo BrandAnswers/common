3631e8b983dba8b5fe55ef7cbc81dd120e400cfb
rahilsrhn <rahilsonusrhn@gmail.com>
2023-02-26 13:38:00 +0400

    [URP-75]-[URP-76] made createdAt to Instant

--- src/main/java/com/suntec/retail/commonDto/CommentDto.java
+++ src/main/java/com/suntec/retail/commonDto/CommentDto.java
@@ -2,9 +2,11 @@ package com.suntec.retail.commonDto;
 
 import lombok.Data;
 
+import java.time.Instant;
+
 @Data
 public class CommentDto {
     String commentedBy;
     String comment;
-    Integer createdAt;
+    Instant createdAt;
 }
